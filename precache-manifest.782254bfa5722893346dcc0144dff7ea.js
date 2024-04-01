96325aed2f353110f84366ffba909c106e1e99ec
0xKufre <90410673+0xKufre@users.noreply.github.com>
2024-04-01 23:05:25 +0100

    Add files via upload

---
+++ precache-manifest.782254bfa5722893346dcc0144dff7ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a41d7f36ca53df324ee1b8b42db5a19a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a45a1b60a878ef0af0cc",
+    "url": "/static/css/main.ba0e73e3.chunk.css"
+  },
+  {
+    "revision": "377bb4087a31e3a1b14a",
+    "url": "/static/js/2.fb041cbc.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.fb041cbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a45a1b60a878ef0af0cc",
+    "url": "/static/js/main.22b06b7a.chunk.js"
+  },
+  {
+    "revision": "e0165f2025b2ccf828a1",
+    "url": "/static/js/runtime-main.b8ac7ad2.js"
+  }
+]);
---
+++ static/js/main.22b06b7a.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonppwa_react_video=this.webpackJsonppwa_react_video||[]).push([[0],{13:function(e,t,a){e.exports=a(36)},35:function(e,t,a){},36:function(e,t,a){"use strict";a.r(t);var n=a(1),r=a.n(n),c=a(11),s=a.n(c);a(12);a(35);var o=()=>{const[e,t]=Object(n.useState)(""),[a,c]=Object(n.useState)({});return r.a.createElement("iframe",{src:"https://real-estate-html-seven.vercel.app",className:"renderHtml",scrolling:"auto"})};s.a.render(r.a.createElement(o,null),document.getElementById("root"))}},[[13,1,2]]]);
+//# sourceMappingURL=main.22b06b7a.chunk.js.map
